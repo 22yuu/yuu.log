749250e5fa91895558c96b0565aac7ece1204e9a
22yuu <22yuu@naver.com>
2023-09-24 23:18:05 +0900

    feat: 하드코딩된 디비정보 환경변수로 변경

--- server/database/maria.js
+++ server/database/maria.js
@@ -1,10 +1,13 @@
 import mariadb from 'mariadb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const CONNECTION_INFO = {
-  host: 'localhost',
-  user: 'root',
-  password: 'wlsdb11@@',
-  database: 'myblog',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 };
 
 export default class MariaDB {
