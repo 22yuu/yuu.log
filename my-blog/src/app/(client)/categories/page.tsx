61d0e323397ad72c0611d40a8440ca69c1820331
yuu <22yuu@naver.com>
2023-09-27 17:56:30 +0900

    feat: Mock 카테고리로부터 데이터 가져오기

--- my-blog/src/app/(client)/categories/page.tsx
+++ my-blog/src/app/(client)/categories/page.tsx
@@ -1,8 +1,29 @@
-import MainWrapper from '@/components/layouts/MainWrapper';
-import { getCategories } from '@/utils/file/file';
+import MainWrapper from "@/components/layouts/MainWrapper";
+import {
+  MockCategoryKeysType,
+  categoryList,
+} from "@/constants/mock-categories";
 
 export default function CatregoriesPage() {
-  const categories = getCategories();
+  const mainCategories: MockCategoryKeysType = Object.keys(categoryList);
 
-  return <MainWrapper>카테고리</MainWrapper>;
+  return (
+    <MainWrapper>
+      <ul>
+        {mainCategories.map((mainCategory) => {
+          return (
+            <li>
+              {mainCategory}
+              <ul>
+                {mainCategory.length > 0 &&
+                  categoryList[mainCategory].map((subCategories) => (
+                    <li className="ml-2">{subCategories}</li>
+                  ))}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </MainWrapper>
+  );
 }
