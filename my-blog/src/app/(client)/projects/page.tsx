7b7ce51258df0a5948da455aded0de9e5d22b21a
22yuu <22yuu@naver.com>
2023-09-30 14:17:13 +0900

    feat: Projects UI 구현 및 스타일링 구현

--- my-blog/src/app/(client)/projects/page.tsx
+++ my-blog/src/app/(client)/projects/page.tsx
@@ -1,5 +1,153 @@
 import MainWrapper from '@/components/layouts/MainWrapper';
+import EyeIcon from '@/components/ui/icons/EyeIcon';
+import ForkIcon from '@/components/ui/icons/ForkIcon';
+import StarIcon from '@/components/ui/icons/StarIcon';
+import Image from 'next/image';
 
 export default function page() {
-  return <MainWrapper>Projects</MainWrapper>;
+  return (
+    <MainWrapper>
+      <div className="mt-12 text-theme-text">
+        <ul className="relative">
+          <div className="absolute left-[80px] w-1 h-full bg-theme-primary"></div>
+          <li className="flex min-h-[150px] mb-24">
+            <div className="flex flex-shrink-0 items-center justify-center rounded-full border-[7px] border-theme-primary w-40 h-40 bg-white z-50">
+              <Image
+                className="rounded-2xl max-w-[120px] max-h-[120px]"
+                src="/images/plip.png"
+                alt="thumbnail"
+                layout="responsive"
+                width={120}
+                height={120}
+                objectFit="fill"
+              />
+            </div>
+            <div className="flex flex-col p-4">
+              <h2 className="mb-2">Project Tilte</h2>
+              <p className="mb-2">
+                This is Project Description... What is your application service
+                please explain it. and What is difficult implmentaion in your
+                project.
+              </p>
+              <div className="flex gap-4">
+                <div className="flex items-center gap-2">
+                  <StarIcon /> <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <ForkIcon />
+                  <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <EyeIcon />
+                  <span>0</span>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="flex min-h-[150px] mb-24">
+            <div className="flex flex-shrink-0 items-center justify-center rounded-full border-[7px] border-theme-primary w-40 h-40 bg-white z-50">
+              <Image
+                className="rounded-2xl max-w-[120px] max-h-[120px]"
+                src="/images/plip.png"
+                alt="thumbnail"
+                layout="responsive"
+                width={120}
+                height={120}
+                objectFit="fill"
+              />
+            </div>
+            <div className="flex flex-col p-4">
+              <h2 className="mb-2">Project Tilte</h2>
+              <p className="mb-2">
+                This is Project Description... What is your application service
+                please explain it. and What is difficult implmentaion in your
+                project.
+              </p>
+              <div className="flex gap-4">
+                <div className="flex items-center gap-2">
+                  <StarIcon /> <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <ForkIcon />
+                  <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <EyeIcon />
+                  <span>0</span>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="flex min-h-[150px] mb-24">
+            <div className="flex flex-shrink-0 items-center justify-center rounded-full border-[7px] border-theme-primary w-40 h-40 bg-white z-50">
+              <Image
+                className="rounded-2xl max-w-[120px] max-h-[120px]"
+                src="/images/plip.png"
+                alt="thumbnail"
+                layout="responsive"
+                width={120}
+                height={120}
+                objectFit="fill"
+              />
+            </div>
+            <div className="flex flex-col p-4">
+              <h2 className="mb-2">Project Tilte</h2>
+              <p className="mb-2">
+                This is Project Description... What is your application service
+                please explain it. and What is difficult implmentaion in your
+                project.
+              </p>
+              <div className="flex gap-4">
+                <div className="flex items-center gap-2">
+                  <StarIcon /> <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <ForkIcon />
+                  <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <EyeIcon />
+                  <span>0</span>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="flex min-h-[150px] mb-24">
+            <div className="flex flex-shrink-0 items-center justify-center rounded-full border-[7px] border-theme-primary w-40 h-40 bg-white z-50">
+              <Image
+                className="rounded-2xl max-w-[120px] max-h-[120px]"
+                src="/images/plip.png"
+                alt="thumbnail"
+                layout="responsive"
+                width={120}
+                height={120}
+                objectFit="fill"
+              />
+            </div>
+            <div className="flex flex-col p-4">
+              <h2 className="mb-2">Project Tilte</h2>
+              <p className="mb-2">
+                This is Project Description... What is your application service
+                please explain it. and What is difficult implmentaion in your
+                project.
+              </p>
+              <div className="flex gap-4">
+                <div className="flex items-center gap-2">
+                  <StarIcon /> <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <ForkIcon />
+                  <span>0</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <EyeIcon />
+                  <span>0</span>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </MainWrapper>
+  );
 }
