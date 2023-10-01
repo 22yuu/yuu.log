609128ffb10f0ebc9ee5f9b94f4f080dd9850332
22yuu <22yuu@naver.com>
2023-10-01 23:45:27 +0900

    feat: Heading atom 컴포넌트 구현 및 각 페이지마다 Heading 컴포넌트 적용

---
+++ my-blog/src/components/ui/atoms/Heading.tsx
@@ -0,0 +1,41 @@
+import cn from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { HTMLAttributes } from 'react';
+
+const variants = cva(
+  `font-semibold smooth duration-300 transition-all mt-12 mb-6 text-theme-text`,
+  {
+    variants: {
+      size: {
+        default: 'text-base',
+        sm: 'text-sm',
+        md: 'text-lg',
+        lg: 'text-xl',
+        xl: 'text-2xl',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+    },
+  }
+);
+
+interface HeadingProps
+  extends HTMLAttributes<HTMLHeadElement>,
+    VariantProps<typeof variants> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Heading({
+  children,
+  size,
+  className,
+  ...attributes
+}: HeadingProps) {
+  return (
+    <h2 className={cn(variants({ size }), className)} {...attributes}>
+      {children}
+    </h2>
+  );
+}
