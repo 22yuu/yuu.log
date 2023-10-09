821d037b5ff6f37a560bc75a3ca0e68d1779008c
yuu <22yuu@naver.com>
2023-10-09 18:55:14 +0900

    chore: Calendar 부분 하단 부분 고정 css 수정

--- my-blog/src/components/ContentSidePanel.tsx
+++ my-blog/src/components/ContentSidePanel.tsx
@@ -3,30 +3,31 @@ import Calendar from "./Calendar";
 
 export default function ContentSidePanel() {
   return (
-    <div className="hidden flex-col right-0 lg:flex max-w-[300px] w-full px-8 pt-6">
-      <div className="sticky top-24">
-        <div className="flex flex-col w-full text-theme-text border-l-2 border-l-theme-text pl-4">
-          <h2 className="font-semibold ">Recently Posts</h2>
-          <ul className="text-sm">
-            <li className="my-2">
-              <Link href="#">this is post hehe</Link>
-            </li>
-            <li className="my-2">
-              <Link href="#">this is post hehe</Link>
-            </li>
-            <li className="my-2">
-              <Link href="#">this is post hehe</Link>
-            </li>
-            <li className="my-2">
-              <Link href="#">this is post hehe</Link>
-            </li>
-            <li className="my-2">
-              <Link href="#">this is post hehe</Link>
-            </li>
-          </ul>
+    <div className="hidden flex-col w-[300px] h-screen px-8 pt-6 right-0 lg:flex max-w-[300px] ">
+      <div className="fixed top-24 h-full">
+        <div className="flex flex-1 flex-col h-full">
+          <div className="flex flex-col w-full text-theme-text border-l-2 border-l-theme-text pl-4">
+            <h2 className="font-semibold ">Recently Posts</h2>
+            <ul className="text-sm">
+              <li className="my-2">
+                <Link href="#">this is post hehe</Link>
+              </li>
+              <li className="my-2">
+                <Link href="#">this is post hehe</Link>
+              </li>
+              <li className="my-2">
+                <Link href="#">this is post hehe</Link>
+              </li>
+              <li className="my-2">
+                <Link href="#">this is post hehe</Link>
+              </li>
+              <li className="my-2">
+                <Link href="#">this is post hehe</Link>
+              </li>
+            </ul>
+          </div>
+          <Calendar />
         </div>
-
-        <Calendar />
       </div>
     </div>
   );
