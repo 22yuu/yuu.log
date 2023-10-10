3035909d8c28cbcfb9a9a03daf764643d3812905
22yuu <22yuu@naver.com>
2023-10-10 23:24:58 +0900

    feat: about 페이지 마크업

--- my-blog/src/app/(admin)/admin/page.tsx
+++ my-blog/src/app/(admin)/admin/page.tsx
@@ -1,17 +1,35 @@
+import Hero from '@/components/Hero';
+import ToggleDarkModeBtn from '@/components/ToggleDarkModeBtn';
 import Login from '@/components/admin/Login';
+import Heading from '@/components/ui/atoms/Heading';
 
 export default function page() {
   const user = true;
+  const animateSidePanelHidden =
+    ' transform -translate-x-full transition-transform ease-in duration-300';
 
   if (!user) {
     return <Login />;
   }
 
   return (
-    <div className="flex text-theme-text">
+    <div className="flex w-full h-full text-theme-text">
       {/* admin side panel */}
-      <div className="w-[200px] h-full bg-theme-primary"></div>
-      <p>Admin Page</p>
+      <div className="fixed w-[350px] h-full bg-theme-primary transform -translate-x-full transition-transform ease-in duration-300 md:translate-x-0"></div>
+      {/* contents */}
+      <div className="w-full px-16 py-8 md:ml-[350px]">
+        <div className="flex justify-between mb-8 border border-red-500">
+          <Heading size={'xl'}>Dash Board</Heading>
+          <div className="flex items-center gap-4">
+            <span>Welcome! {user ? 'Admin' : 'Guest'}</span>
+            <ToggleDarkModeBtn />
+          </div>
+        </div>
+        <div className="h-[300px] mb-8 border border-red-500"></div>
+        <div className="h-[300px] mb-8 border border-red-500"></div>
+        <div className="h-[300px] mb-8 border border-red-500"></div>
+        <div className="h-[300px] mb-8 border border-red-500"></div>
+      </div>
     </div>
   );
 }
