4051a2497d4aee161aabd58bad06205148b70973
22yuu <22yuu@naver.com>
2023-10-19 01:12:59 +0900

    feat: 어드민 페이지 차트 UI 구현 완료

--- my-blog/src/app/(admin)/admin/page.tsx
+++ my-blog/src/app/(admin)/admin/page.tsx
@@ -1,74 +1,124 @@
-import Hero from '@/components/Hero';
-import ToggleDarkModeBtn from '@/components/ToggleDarkModeBtn';
-import Login from '@/components/admin/Login';
-import Heading from '@/components/ui/atoms/Heading';
-import ChartIcon from '@/components/ui/icons/ChartIcon';
-import ContentIcon from '@/components/ui/icons/ContentIcon';
-import Link from 'next/link';
+'use client';
+
+import React from 'react';
+import ContentHeader from '../components/ContentHeader';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Tick,
+  ChartOptions,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import getRandomInt from '@/utils/getRandomInt';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip
+);
+
+const labels = [
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+  30,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  '오늘',
+];
+
+export const options: ChartOptions<'line'> = {
+  responsive: true,
+  maintainAspectRatio: false, // 비율 고정
+  scales: {
+    x: {
+      ticks: {
+        color: '#A1A1AA',
+      },
+      grid: {
+        display: false,
+      },
+    },
+
+    y: {
+      ticks: {
+        color: '#A1A1AA',
+      },
+      grid: {
+        color: function (context) {
+          if (context.tick.value > 0) {
+            return '#a1a1aa';
+          }
+        },
+      },
+      border: {
+        dash: [2, 4],
+      },
+    },
+  },
+};
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: '방문자 수',
+      data: labels.map(() => getRandomInt(30)),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
+};
 
 export default function page() {
-  const user = true;
-
-  if (!user) {
-    return <Login />;
-  }
-
   return (
-    <div className="flex w-full h-full text-theme-text">
-      {/* admin side panel */}
-      <div className="fixed w-[350px] h-full bg-theme-primary transform -translate-x-full transition-transform ease-in duration-300 md:translate-x-0">
-        <div className="w-full text-center mt-12">
-          <Heading size={'xl'}>Yuu.log</Heading>
-          <div className="p-4">
-            <button className="w-full border border-zinc-500 rounded-xl bg-theme-secondary dark:bg-theme-primary text-white dark:text-theme-text py-2">
-              글 작성 하기
-            </button>
-          </div>
-        </div>
-        <div>
-          <div className="mb-4">
-            <div className="flex pl-6 py-2 items-center">
-              <ChartIcon className="mr-2" />
-              <Heading size={'md'}>통계</Heading>
-            </div>
-            <ul>
-              <li className="pt-3 pl-12">
-                <Link href="/">방문 통계</Link>
-              </li>
-              <li className="pt-3 pl-12">
-                <Link href="/">유입 경로</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="mb-4">
-            <div className="flex pl-6 py-2 items-center">
-              <ContentIcon className="mr-2" />
-              <Heading size={'md'}>콘텐츠</Heading>
-            </div>
-            <ul>
-              <li className="pt-3 pl-12">
-                <Link href="/">글 관리</Link>
-              </li>
-              <li className="pt-3 pl-12">
-                <Link href="/">카테고리 관리</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <div className="w-full px-16 mt-12 md:ml-[350px]">
+      <ContentHeader />
+      <div className="w-full flex gap-8 px-6 py-8 border border-zinc-300 dark:border-zinc-500 bg-theme-bg">
+        <dl>
+          <dt className="text-sm">오늘 방문수</dt>
+          <dd className="font-semibold text-xl">0</dd>
+        </dl>
+        <dl>
+          <dt className="text-sm">어제 방문수</dt>
+          <dd className="font-semibold text-xl">0</dd>
+        </dl>
+        <dl>
+          <dt className="text-sm">누적 방문수</dt>
+          <dd className="font-semibold text-xl">0</dd>
+        </dl>
       </div>
-      {/* contents */}
-      <div className="w-full px-16 mt-12 md:ml-[350px]">
-        <div className="flex justify-between mb-8 border border-red-500">
-          <Heading size={'xl'}>Dash Board</Heading>
-          <div className="flex items-center gap-4">
-            <span>Welcome! {user ? 'Admin' : 'Guest'}</span>
-            <ToggleDarkModeBtn />
-          </div>
-        </div>
-        <div className="h-[300px] mb-8 border border-red-500"></div>
-        <div className="h-[300px] mb-8 border border-red-500"></div>
-        <div className="h-[300px] mb-8 border border-red-500"></div>
-        <div className="h-[300px] mb-8 border border-red-500"></div>
+      <div className="w-full h-80 px-6 py-8 border border-zinc-300 dark:border-zinc-500 bg-theme-bg mt-4">
+        <Line options={options} data={data} />
       </div>
     </div>
   );
---
+++ my-blog/src/app/(admin)/components/ContentHeader.tsx
@@ -0,0 +1,25 @@
+import ToggleDarkModeBtn from '@/components/ToggleDarkModeBtn';
+import Login from '@/components/admin/Login';
+import Heading from '@/components/ui/atoms/Heading';
+import React from 'react';
+
+type Props = {
+  title?: string;
+};
+
+export default function ContentHeader({ title = '방문 통계' }: Props) {
+  const user = true;
+
+  if (!user) {
+    return <Login />;
+  }
+  return (
+    <div className="flex justify-between mb-8">
+      <Heading size={'xl'}>{title}</Heading>
+      <div className="flex items-center gap-4">
+        <span>Welcome! {user ? 'Admin' : 'Guest'}</span>
+        <ToggleDarkModeBtn />
+      </div>
+    </div>
+  );
+}
