d62f7b54722531855cf7b3e719cd491567c4bebf
22yuu <22yuu@naver.com>
2023-11-07 23:33:05 +0900

    feat: 마크다운 에디터 수정 - 마크다운 파서 markdown-it 사용

--- my-blog/src/app/(admin)/admin/posts/write/page.tsx
+++ my-blog/src/app/(admin)/admin/posts/write/page.tsx
@@ -1,26 +1,34 @@
 'use client';
 
-import { writePost } from '@/api/post';
 import ContentHeader from '@/app/(admin)/components/ContentHeader';
 import MarkdownViewer from '@/components/MarkdownViewer';
+import MarkdownIt from 'markdown-it';
 import Heading from '@/components/ui/atoms/Heading';
 import { LoginContextProps, useLoginContext } from '@/contexts/LoginProvider';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 export default function WritePage() {
   const {
     user: { accessToken },
   } = useLoginContext() as LoginContextProps;
-  const ref = useRef<HTMLDivElement | null>(null);
+  const editorRef = useRef<HTMLDivElement | null>(null);
+  const previewRef = useRef<HTMLDivElement | null>(null);
   const [markdown, setMarkDown] = useState('');
-  const [imgSrc, setImgSrc] = useState('');
+  const mdParser = new MarkdownIt({
+    html: false,
+    linkify: true,
+    breaks: true,
+  });
 
   const onSubmitHandler = async () => {
     // const res = await writePost(accessToken!, text!);
   };
 
   const onInputTextHandler = (e: React.ChangeEvent<HTMLDivElement>) => {
-    setMarkDown(ref.current?.innerHTML as string);
+    setMarkDown(editorRef.current?.innerText as string);
+    if (markdown.length > 0) {
+      previewRef.current!.innerHTML = mdParser.render(markdown);
+    }
   };
 
   const onPasteImageHandler = async (e: any) => {
@@ -30,7 +38,7 @@ export default function WritePage() {
     if (item.type.includes('image')) {
       // 클립보드로부터 이미지를 가져오는 방법 1
       const imgSrc = URL.createObjectURL(await item.getAsFile());
-      ref.current!.innerHTML += `<div><img src="${imgSrc}"></div>`;
+      editorRef.current!.innerText += `![](${imgSrc})`;
       // URL.revokeObjectURL(imgSrc);
 
       // 클립보드로부터 이미지를 가져오는 방법 2
@@ -38,7 +46,7 @@ export default function WritePage() {
 
       // reader.onload = (e) => {
       //   console.log('image loaded!!!');
-      //   ref.current!.innerHTML += `<div><img src=${e.target?.result} ></div>`;
+      //   editorRef.current!.innerHTML += `<div><img src=${e.target?.result} ></div>`;
       // };
 
       // reader.readAsDataURL(item.getAsFile());
@@ -51,7 +59,7 @@ export default function WritePage() {
       <div className="flex w-full">
         <div className="w-[50%] mr-8">
           <div
-            ref={ref}
+            ref={editorRef}
             className="w-full h-full bg-transparent outline-none"
             contentEditable={true}
             placeholder="포스트 내용 입력하기"
@@ -62,7 +70,9 @@ export default function WritePage() {
         </div>
         <div>
           <Heading size="sm">미리보기</Heading>
-          {markdown && <MarkdownViewer content={markdown} />}
+          {markdown && (
+            <div ref={previewRef} className="prose text-theme-text"></div>
+          )}
         </div>
       </div>
       <button className="self-end" onClick={onSubmitHandler}>
